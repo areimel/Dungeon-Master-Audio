81ad0ed60ffa83127941746a99de718ef7f50afb
Alec Reimel <alecreimel1@gmail.com>
2020-07-29 20:53:24 -0400

    minor

--- js/script.js
+++ js/script.js
@@ -465,3 +465,29 @@ $(document).ready(function(){
 
 
 });
+
+
+/********************************************
+
+GA AUTO-TAGGER CODE
+
+********************************************/
+$(document).ready(function(){
+	/***** NAV *****/
+		var category	= "Nav";
+		var action		= "Click";
+		var value		= "NULL";
+
+		$('nav a').autotagger(category, action, value);
+
+	/***** FOOTER *****/
+		var category	= "Footer";
+		var action		= "Click";
+		var value		= "NULL";
+
+		$('footer a').autotagger(category, action, value);
+
+	/***** INITIALIZE *****/
+		$('html').eventfire_ready();
+
+});
